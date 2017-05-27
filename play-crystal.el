b2bfac9868ede51a25535a1f0aa8df01e35d4070
Vitalii Elenhaupt <velenhaupt@gmail.com>
2017-05-27 20:01:52 +0300

    Add insert & insert-another-buffer

--- play-crystal.el
+++ play-crystal.el
@@ -28,10 +28,21 @@
 ;;
 ;; https://play.crystal-lang.org integration.
 ;;
-;; * [x] Allows to fetch code into Emacs buffer
-;; * [ ] Allows to create crystal and execute Crystal code at [play.crystal-lang.org](https://play.crystal-lang.org)
+;; ### Features:
+;;
+;; * [x] Allows to paste code into Emacs buffers
+;; * [ ] Allows to submit code to [play.crystal-lang.org](https://play.crystal-lang.org)
+;;
+;; ### Usage
+;;
+;; Run one of the predefined interactive functions to fetch or submit
+;; code from [play.crystal-lang.org](https://play.crystal-lang.org).
+;;
 
 ;;; Code:
+
+(eval-when-compile
+  (require 'cl))
 
 (require 'json)
 (require 'request)
@@ -44,23 +55,28 @@
 
 (defconst play-crystal-version "0.1.0")
 (defconst play-crystal-baseurl "https://play.crystal-lang.org")
+(defconst play-crystal-runs-path "/runs")
 
-(defvar play-crystal-debug-buffer-name "*Play-Crystal*"
-  "Buffer name responses from play.crystal-lang.org.")
+(defvar play-crystal-buffer-name "*Play-Crystal*"
+  "Buffer name for code insertions.")
+
+(defvar play-crystal-debug-buffer-name "*Play-Crystal-Debug*"
+  "Buffer name for responses from play.crystal-lang.org.")
 
 (defconst play-crystal-default-headers
   '(("Accept" . "application/json")
     ("User-Agent" . ,(format "play-crystal.el/%s" play-crystal-version))))
 
-(cl-defun play-crystal-request
+(cl-defun play-crystal--request
     (endpoint
      &key
      (type "GET")
      (params nil)
      (data nil)
      (parser 'json-read)
-     (error 'play-crystal-default-callback)
-     (success 'play-crystal-default-callback)
+     (error nil)
+     (success nil)
+     (complete 'play-crystal--default-callback)
      (headers play-crystal-default-headers)
      (timeout nil)
      (sync nil)
@@ -74,14 +90,18 @@
            :headers headers
            :parser parser
            :success success
+           :complete complete
            :error error
            :timeout timeout
            :status-code status-code
            :sync sync))
 
-(cl-defun play-crystal-default-callback (&key data response error-thrown &allow-other-keys)
+(cl-defun play-crystal--default-callback (&key data response error-thrown &allow-other-keys)
   (with-current-buffer (get-buffer-create play-crystal-debug-buffer-name)
-    (and error-thrown (message (error-message-string error-thrown)))
+    (let ((error-message (assoc-default 'message (assoc-default 'error data))))
+      (if (not (s-blank-str? error-message))
+          (message error-message)
+        (and error-thrown (message (error-message-string error-thrown)))))
     (let ((inhibit-read-only t))
       (erase-buffer)
       (and (stringp data) (insert data))
@@ -89,16 +109,55 @@
         (unless (or (null raw-header) (string-empty-p raw-header))
           (insert "\n" raw-header))))))
 
-(defun play-crystal-insert-run (id)
-  "Insert play crystal run code into a current buffer. ID is a crystal run identity."
-  (interactive
-   (list
-    (read-string "Enter run id: ")))
-  (play-crystal-request
-   (format "/runs/%s" id)
+(cl-defun play-crystal--chunk (data)
+  "Pre-formatted play crystal code chunk."
+  (let* ((run (assoc-default 'run data))
+         (html-url (assoc-default 'html_url run))
+         (created-at (assoc-default 'created_at run))
+         (language (assoc-default 'language run))
+         (version (assoc-default 'version run))
+         (exit-code (assoc-default 'exit_code run))
+         (code (assoc-default 'code run))
+         (id (assoc-default 'id run)))
+    (when (not (s-blank-str? code))
+      (concat
+       (format "\n# Html url: %s" html-url)
+       (format "\n# Created at: %s" created-at)
+       (format "\n# Language: %s" language)
+       (format "\n# Version: %s" version)
+       (format "\n# Exit code: %d" exit-code)
+       (format "\n%s" code)
+       (format "\n# End of chunk #%s" id)))))
+
+(cl-defun play-crystal--read-run-id ()
+  "Read run id."
+  (list (read-string "Enter run id: ")))
+
+(cl-defun play-crystal--run-path (run-id)
+  "Returns a path to the run."
+  (format "%s/%s" play-crystal-runs-path run-id))
+
+(defun play-crystal-insert (run-id)
+  "Insert code defined by RUN-ID into the current buffer."
+  (interactive (play-crystal-read-run-id))
+  (play-crystal--request
+   (play-crystal--run-path run-id)
    :success (cl-function
              (lambda (&key data &allow-other-keys)
-               (insert (assoc-default 'code (assoc-default 'run data)))))))
+               (insert (play-crystal-chunk data))))))
+
+(defun play-crystal-insert-another-buffer (run-id)
+  "Insert code defined by RUN-ID into another buffer."
+  (interactive (play-crystal-read-run-id))
+  (play-crystal--request
+   (play-crystal--run-path run-id)
+   :success (cl-function
+             (lambda (&key data &allow-other-keys)
+               (progn
+                 (switch-to-buffer play-crystal-buffer-name)
+                 (erase-buffer)
+                 (when (fboundp 'crystal-mode) (crystal-mode))
+                 (insert (play-crystal-chunk data)))))))
 
 (provide 'play-crystal)
 ;;; play-crystal.el ends here
